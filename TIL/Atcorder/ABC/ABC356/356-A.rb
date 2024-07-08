80adc82d3d3414e60bf1b704aae932e230f8e8bd
TechMishima <t.mishima242@gmail.com>
2024-07-09 00:48:18 +0900

    7/9

--- TIL/Atcorder/ABC/ABC356/356-A.rb
+++ TIL/Atcorder/ABC/ABC356/356-A.rb
@@ -4,6 +4,15 @@ n, l, r = gets.split.map(&:to_i)
 
 line = []
 n.times do |i|
-  line << i
+  line << i + 1
 end
 
+start_index = l - 1
+end_index = r - 1
+# 指定範囲を逆にする
+reversed_subarray = line[start_index ..end_index].reverse
+
+# 元の配列の範囲を置き換える
+line[start_index..end_index] = reversed_subarray
+
+puts line
